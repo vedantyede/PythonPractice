a9cb08d3d29d8520459767de3fb521d82a740b01
vedantyede <vedantyede@gmail.com>
2023-02-24 10:13:26 +0530

    24-feb commit

--- SelfPractice/Matrix1.py
+++ SelfPractice/Matrix1.py
@@ -1,7 +1,14 @@
 # Create a Matrix with for loop
 
-list = []
+listItems = []
 for i in range(3):
     for j in range(3):
-        list.append(i*j)
-print(list)
+        listItems.append(i * j)
+print(listItems)  # [0, 0, 0, 0, 1, 2, 0, 2, 4]
+
+listItems = [(i, j) for i in range(3) for j in range(3)]
+print(i, j)  # 2 2
+print(listItems)  # [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+
+listItems = [(i, j, k) for i in range(2) for j in range(2) for k in range(2)]
+print(listItems)  # [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
